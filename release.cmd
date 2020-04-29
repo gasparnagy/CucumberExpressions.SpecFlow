c6cdeac2c1045bee4b1afa31eec5362cfd29c9fa
Gaspar Nagy <gaspar.nagy@gmail.com>
2020-04-29 07:42:50 +0200

    Fix package details & release

---
+++ release.cmd
@@ -0,0 +1,23 @@
+@pushd %~dp0
+
+set PRODVER=%1
+set CONFIG_VAL=Release
+
+@echo publishing version %PRODVER%, %CONFIG%, OK?
+@pause
+
+dotnet build -c "%CONFIG_VAL%" -p:PackageVersion=%PRODVER% -p:Version=%PRODVER%
+IF ERRORLEVEL 1 GOTO error
+
+copy /Y CucumberExpressions.SpecFlow.SpecFlowPlugin\bin\%CONFIG_VAL%\CucumberExpressions.SpecFlow.3-1.%PRODVER%.nupkg %NUGET_LOCAL_FEED%\CucumberExpressions.SpecFlow.3-1.%PRODVER%.nupkg
+IF ERRORLEVEL 1 GOTO error
+
+@popd
+goto end
+
+:error
+echo ERROR!!!
+@popd
+exit /b
+
+:end
