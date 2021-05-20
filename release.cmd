b70bc77526769ef7e374015df411839ec513702f
Gaspar Nagy <gaspar.nagy@gmail.com>
2021-05-20 16:07:55 +0200

    Upgrade to support SpecFlow v3.8

--- release.cmd
+++ release.cmd
@@ -9,7 +9,7 @@ set CONFIG_VAL=Release
 dotnet build -c "%CONFIG_VAL%" -p:PackageVersion=%PRODVER% -p:Version=%PRODVER%
 IF ERRORLEVEL 1 GOTO error
 
-copy /Y CucumberExpressions.SpecFlow.SpecFlowPlugin\bin\%CONFIG_VAL%\CucumberExpressions.SpecFlow.3-7.%PRODVER%.nupkg %NUGET_LOCAL_FEED%\CucumberExpressions.SpecFlow.3-6.%PRODVER%.nupkg
+copy /Y CucumberExpressions.SpecFlow.SpecFlowPlugin\bin\%CONFIG_VAL%\CucumberExpressions.SpecFlow.3-8.%PRODVER%.nupkg %NUGET_LOCAL_FEED%\CucumberExpressions.SpecFlow.3-8.%PRODVER%.nupkg
 IF ERRORLEVEL 1 GOTO error
 
 @popd
